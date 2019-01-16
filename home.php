48291dafb8960a217c9ef2f6177a51edc4678e95
armjubjub <32847689+armjubjub@users.noreply.github.com>
2019-01-17 00:21:29 +0700

    add file home.php
    add file

---
+++ home.php
@@ -0,0 +1,24 @@
+<html>
+<head Main></head>
+<link rel="stylesheet" type="text/css" href="dc.css">
+<body background = bgg.jpg>
+<table  >
+<th width=80> 
+<a href = home.php><img src = home.png width=100%></a>
+</th>
+<th width=100>
+
+<div class="dropdown">
+  <button class="dropbtn">ตัวเลือก</button>
+  <div class="dropdown-content">
+    <a href="profile.php">ประวัติส่วนตัว</a>
+    <a href="education.php">ประวัติการศึกษา</a>
+    <a href="introduce.php">ข้อมูลทั่วไป</a>
+  </div>
+</div> 
+</th>
+</table>
+<br><br><br><br><br><br>
+<center><img src = well.png></center>
+</body>
+</html>
